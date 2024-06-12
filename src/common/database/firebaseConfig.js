b6d1bb98cd89324e7b01e4184e1598866205ad05
Mario <mh@dtox.ai>
2024-06-11 19:22:53 -0500

    Ajustes en variables de entorno / protegidas

--- src/common/database/firebaseConfig.js
+++ src/common/database/firebaseConfig.js
@@ -1,11 +1,31 @@
+// const admin = require('firebase-admin');
+// const serviceAccount = require('./key.json');
+
+// // Inicializar Firebase Admin SDK
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: 'https://dtox-ai.firebaseio.com'
+
+// });
+
+// console.log("Firebase Admin SDK cargado correctamente.");
+
+// const db = admin.firestore();
+
+// module.exports = db;
+
+
 const admin = require('firebase-admin');
-const serviceAccount = require('./key.json');
 
-// Inicializar Firebase Admin SDK
+// Inicializar Firebase Admin SDK con las credenciales de las variables de entorno
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://dtox-ai.firebaseio.com'
-
+    credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Ajustar el formato de la clave privada
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    clientId: process.env.FIREBASE_CLIENT_ID
+  }),
+  databaseURL: process.env.FIREBASE_DATABASE_URL
 });
 
 console.log("Firebase Admin SDK cargado correctamente.");
--- src/common/services/messageService.js
+++ src/common/services/messageService.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+
 const languageAbbreviations = require('../utils/languageAbr');
 const userService = require('./userService');
 const sendMessage = require('./sendMessage');
@@ -28,6 +28,11 @@ exports.processMessage = async (body) => {
         case 'pregunta_terciaria':
           await handleNameRequest(senderId, receivedMessage, estado);
           break;
+          case 'consentimientocuestionario':
+          case 'pregunta_secundaria_cuestionario':
+          await handleConsentResponse(senderId, receivedMessage);
+          break;
+
       }
     }
   }
@@ -49,8 +54,8 @@ const handleLanguageSelection = async (senderId, receivedMessage) => {
     userContext[senderId] = { idioma: languageCode, estado: 'felicitaciones' };
 
     const welcomeMessage = languageCode === 'ingles'
-      ? 'Welcome to DTOX.Ai, the smartest virtual assistant that will help you quit vaping'
-      : 'Bienvenidx a DTOX.Ai, el asistente virtual más inteligente que te ayudará a dejar de vapear';
+      ? 'Hello! I am Olya, the smartest virtual assistant that will help you quit vaping'
+      : '¡Hola! Soy Olya, el asistente virtual más inteligente que te ayudará a dejar de vapear';
     const congratulationsMessage = languageCode === 'ingles'
       ? 'You have taken the first step towards a healthier life, so Congratulations!'
       : 'Has dado el primer paso hacia una vida más saludable, así que ¡Felicidades!';
@@ -79,9 +84,67 @@ const handleNameRequest = async (senderId, receivedMessage) => {
   const nameParts = userName.split(" ");
 
   
-  let user = userContext[senderId]; // Obtener la información del usuario desde el contexto
+  const { language, estado } = await getUserInfo(senderId);
 
-  // Si no hay información del usuario en el contexto, obtenerla desde la base de datos
+  console.log(`Usuario ${senderId} tiene idioma: ${language}`);
+
+
+  if (nameParts.length === 1) {
+    await userService.updateUser(senderId, { nombre: userName, estado: 'plan' });
+
+    await sendMessage(senderId, language === 'ingles'
+      ? `We will start by creating a special plan for you.All Right?`
+      : `Comenzaremos creando un plan especial para tí. Esta bien?`);
+
+      
+    await userService.updateUser(senderId, { estado: 'consentimientocuestionario' });
+
+    // Actualizar el estado en el contexto del usuario
+    userContext[senderId].estado = 'consentimientocuestionario';
+
+
+  } else {
+    const nextState = estado === 'solicitudnombre' ? 'pregunta_secundaria' : 'pregunta_terciaria';
+    await userService.updateUser(senderId, { estado: nextState });
+    console.log(`Usuario ${senderId} actualizado con estado: ${nextState}`);
+    // Actualizar el estado en el contexto del usuario
+    userContext[senderId].estado = nextState;
+
+    const message = nextState === 'pregunta_secundaria'
+      ? (language === 'ingles' ? "Okay... What would you like me to call you?" : "Ok… ¿Cómo prefieres que te diga?")
+      : (language === 'ingles' ? "Oops! I didn't catch that. What do you want me to call you?" : "Opps! No entendí eso. ¿Cómo quieres que te llame?");
+    await sendMessage(senderId, message);
+  }
+};
+
+const handleConsentResponse = async (senderId, receivedMessage) => {
+  const { language, estado } = await getUserInfo(senderId);
+  console.log(`Usuario ${senderId} tiene idioma: ${language} y estado: ${estado}`);
+  
+  // Definimos algunas respuestas afirmativas que pueden indicar consentimiento
+  const affirmativeResponses = ['yes','ok','okey', 'yeah', 'sure', 'of course', 'okay', 'si', 'claro', 'por supuesto'];
+
+  if (affirmativeResponses.includes(receivedMessage.toLowerCase())) {
+    await sendMessage(senderId, language === 'ingles'
+      ? "Great! Let's proceed with the questions."
+      : "¡Genial! Procedamos con las preguntas.");
+
+    // Actualizar el estado después de obtener el consentimiento
+    await userService.updateUser(senderId, { estado: 'consentimientoaceptado' });
+  } else {
+    // En caso de que la respuesta no sea afirmativa
+    await sendMessage(senderId, language === 'ingles'
+      ? "I'm sorry, I didn't catch that. Can we move on to the questions so I can assist you better?"
+      : "Lo siento, no entendí eso. ¿Podemos pasar a las preguntas para que pueda ayudarte mejor?");
+
+    // Actualizar el estado en caso de que el usuario no dé su consentimiento
+    await userService.updateUser(senderId, { estado: 'pregunta_secundaria_cuestionario' });
+  }
+};
+
+async function getUserInfo(senderId) {
+  let user = userContext[senderId];
+
   if (!user) {
     const userData = await userService.getUser(senderId);
     const userDataFields = userData._fieldsProto;
@@ -95,33 +158,5 @@ const handleNameRequest = async (senderId, receivedMessage) => {
   const language = user.idioma;
   const estado = user.estado;
 
-  console.log(`Usuario ${senderId} tiene idioma: ${language}`);
-
-  if (nameParts.length === 1) {
-    await userService.updateUser(senderId, { nombre: userName, estado: 'presentacionbot' });
-    console.log(`Usuario ${senderId} actualizado con nombre: ${userName} y estado: presentacionbot`);
-
-    await sendMessage(senderId, language === 'ingles'
-      ? `Nice to meet you ${userName}! My name is Xoee 010010100101010100101110100100101010101010010 and I will be your personal assistant on this journey.`
-      : `¡Qué gusto ${userName}! Yo me llamo 010010100101010100101110100100101010101010010, y seré tu asistente personal en este viaje.`);
-    await sendMessage(senderId, language === 'ingles'
-      ? `Haha, I'm just kidding! My name is Eli, your quit vaping coach. We will start by creating a special plan for you.`
-      : `Jajaj, sólo estoy bromeando! Me llamo Eli, tu coach para dejar de vapear. Comenzaremos creando un plan especial para tí.`);
-    await userService.updateUser(senderId, { estado: 'consentimientocuestionario' });
-
-    // Actualizar el estado en el contexto del usuario
-    userContext[senderId].estado = 'consentimientocuestionario';
-  } else {
-    const nextState = estado === 'solicitudnombre' ? 'pregunta_secundaria' : 'pregunta_terciaria';
-    await userService.updateUser(senderId, { estado: nextState });
-    console.log(`Usuario ${senderId} actualizado con estado: ${nextState}`);
-
-    // Actualizar el estado en el contexto del usuario
-    userContext[senderId].estado = nextState;
-
-    const message = nextState === 'pregunta_secundaria'
-      ? (language === 'ingles' ? "Okay... What would you like me to call you?" : "Ok… ¿Cómo prefieres que te diga?")
-      : (language === 'ingles' ? "Oops! I didn't catch that. What do you want me to call you?" : "Opps! No entendí eso. ¿Cómo quieres que te llame?");
-    await sendMessage(senderId, message);
-  }
-};
+  return { language, estado };
+}
