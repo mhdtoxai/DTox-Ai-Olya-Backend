e3d95100b16934896851aab1eef8e520eb73fd20
Mario <mh@dtox.ai>
2024-06-26 17:35:53 -0600

    update rest wp

--- src/common/controllers/messageController.js
+++ src/common/controllers/messageController.js
@@ -1,31 +1,15 @@
-
 const messageService = require('../services/messageService');
 
 exports.handleIncomingMessage = async (req, res) => {
-  console.log("Mensaje entrante del webhook:", JSON.stringify(req.body, null, 2));
+  // Enviar respuesta HTTP 200 inmediatamente
+  res.sendStatus(200);
 
-  // Verificar si es un mensaje de estado (statuses)
-  if (req.body.object === "whatsapp_business_account" && req.body.entry && req.body.entry.length > 0) {
-    const entry = req.body.entry[0];
-    if (entry.changes && entry.changes.length > 0 && entry.changes[0].field === "messages") {
-      const message = entry.changes[0].value;
-      if (message.statuses && message.statuses.length > 0) {
-        // Es un mensaje de estado, responder con 200 OK
-        res.sendStatus(200);
-        console.log("Mensaje de estado recibido:", JSON.stringify(message, null, 2));
-        return;
-      }
-    }
-  }
-
-  // Si no es un mensaje de estado, procesarlo como un mensaje normal
-  res.sendStatus(200); // Enviar respuesta HTTP 200 inmediatamente
-
+  // Procesar el mensaje
   try {
     await messageService.processMessage(req.body);
   } catch (error) {
     console.error('Error al procesar el mensaje:', error);
-    res.sendStatus(400); // Responder con 400 en caso de error
+    // No enviar res.sendStatus(400) aquí, ya hemos respondido con 200 OK
   }
 };
 
