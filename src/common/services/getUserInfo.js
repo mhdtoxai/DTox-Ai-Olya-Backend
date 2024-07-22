ce72c1cd6155ceeaa1c06752f2a511cb2137f74e
Mario <mh@dtox.ai>
2024-07-22 15:46:42 -0500

    Pruebas de Time

--- src/common/services/getUserInfo.js
+++ src/common/services/getUserInfo.js
@@ -3,28 +3,32 @@ const userService = require('../services/userService');
 const userContext = require('./userContext');
 
 const getUserInfo = async (senderId) => {
-  let user = userContext[senderId];
+  try {
+    // Obtener datos del usuario desde la base de datos
+    const userData = await userService.getUser(senderId);
+    const userDataFields = userData._fieldsProto;
 
-  if (!user) {
-    try {
-      const userData = await userService.getUser(senderId);
-      const userDataFields = userData._fieldsProto;
+    // // Log completo de userDataFields para depuración
+    // console.log(`Datos completos del usuario obtenidos de la base de datos:`, userDataFields);
 
-      // Verificar que las claves existen antes de acceder a sus valores
-      const idioma = userDataFields.idioma?.stringValue || '';
-      const estado = userDataFields.estado?.stringValue || '';
-      const nombre = userDataFields.nombre?.stringValue || '';
-      const timezone = userDataFields.timezone?.stringValue || '';
+    // Verificar que las claves existen antes de acceder a sus valores
+    const idioma = userDataFields.idioma?.stringValue || '';
+    const estado = userDataFields.estado?.stringValue || '';
+    const nombre = userDataFields.nombre?.stringValue || '';
+    const timezone = userDataFields.timezone?.stringValue || '';
 
-      userContext[senderId] = { idioma, estado, nombre, timezone };
-      user = userContext[senderId];
-    } catch (error) {
-      console.error(`Error al obtener información del usuario ${senderId}:`, error);
-      throw error; // Propagar el error para manejarlo en un nivel superior
-    }
+    // Actualizar el contexto con los datos del usuario y un timestamp
+    userContext[senderId] = { idioma, estado, nombre, timezone, timestamp: Date.now() };
+
+    // Log de cada campo
+    console.log(`Datos del usuario actualizados en el contexto: idioma=${idioma}, estado=${estado}, nombre=${nombre}, timezone=${timezone}`);
+
+    // Retornar la información del usuario
+    return userContext[senderId];
+  } catch (error) {
+    console.error(`Error al obtener información del usuario ${senderId}:`, error);
+    throw error; // Propagar el error para manejarlo en un nivel superior
   }
-
-  return userContext[senderId];
 };
 
 module.exports = getUserInfo;
