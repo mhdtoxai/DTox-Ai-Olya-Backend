cd2efdd4f08dd631bb90a971d35f7ba786669d5e
Mario <mh@dtox.ai>
2024-08-05 14:27:31 -0500

    Test 2

--- src/common/handlers/Onboarding/handleReportVape.js
+++ src/common/handlers/Onboarding/handleReportVape.js
@@ -13,7 +13,7 @@ const handleReportVape = async (senderId) => {
 
    
     // Obtener el score más reciente desde la API
-    const response = await axios.post('https://h83tcbv3-3000.usw3.devtunnels.ms/api/test/testrespiracion/obtenerpruebas', { userId: senderId });
+    const response = await axios.post('https://jjhvjvui.top/api/test/testrespiracion/obtenerpruebas', { userId: senderId });
     const tests = response.data;
     if (tests.length === 0) {
       console.log('No se encontraron tests para el usuario');
