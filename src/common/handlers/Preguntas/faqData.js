77436f245b8b8c621f93a6f493f7cc144e462891
Mario <mh@dtox.ai>
2024-08-28 11:31:47 -0500

    Tester

--- src/common/handlers/Preguntas/faqData.js
+++ src/common/handlers/Preguntas/faqData.js
@@ -146,7 +146,10 @@ const faqs = {
       "qué es olya",
       "cómo funciona olya",
       "Cómo funciona olya",
-      "Cómo funciona ",
+      "Cómo funciona",
+      "Cómo funciona?",
+      "cómo funciona",
+      "como funciona",
       "qué hace olya",
       "explicación de olya",
       "qué es y cómo funciona olya"
