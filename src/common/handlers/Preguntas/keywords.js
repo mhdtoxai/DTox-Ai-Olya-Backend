28d57e0344145cd17cc4053a95b2891784b1c37f
Mario <mh@dtox.ai>
2024-09-25 21:47:04 -0500

    Test2 25-09-24

--- src/common/handlers/Preguntas/keywords.js
+++ src/common/handlers/Preguntas/keywords.js
@@ -2,23 +2,10 @@
 
 const keywords = {
     ingles: [
-      'craving', 'vape', 'hit', 'urge', 'tempted', 'relapse', 'nicotine', 'desperate',
-      'struggling', 'control', 'break', 'buzz', 'cave in', 'fix', 'temptation',
-      'giving up', 'weak', 'lose control', 'desperate for', 'struggling with',
-      'cravings', 'feel like', 'need to', 'about to', 'lose this battle', 'calm down',
-      'fix', 'control over cravings', 'giving in', 'struggling not to', 'need relief',
-      'desperate for a vape', 'overwhelmed', 'losing willpower', 'breaking streak',
-      'need hit', 'resisting', 'losing control', 'feel like breaking', 'temptation is unbearable',
-      'fighting cravings', 'losing', 'control', 'on the edge', 'desperate', 'need vape'
+      'craving'
     ],
     español: [
-      'antojo', 'vapear', 'calada', 'necesito', 'ganas', 'sensación', 'vape', 'desesperado',
-      'relajar', 'controlar', 'ansiedad', 'poco', 'descanso', 'disfrutar', 'pausa', 'inhalación',
-      'bocanada', 'estres', 'tensión', 'vapor', 'momento', 'calmarme', 'relajación', 'vapeo',
-      'desesperado', 'temor', 'tranquilidad', 'calada rápida', 'ansioso', 'vaporizar',
-      'desesperación', 'calada urgente', 'relajación urgente', 'necesito vapear', 'momentos de vapeo',
-      'perder el control', 'romper la racha', 'estrés', 'al borde', 'tenso', 'cansado',
-      'necesito vape', 'relax', 'tensión'
+      'antojo'
     ]
   };
   
